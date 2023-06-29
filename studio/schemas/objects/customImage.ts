6441e39016b44f5769ee7e7fa788fbc16ab0187a
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-06-29 21:00:07 +0700

    📷 Added an image diagram

---
+++ studio/schemas/objects/customImage.ts
@@ -0,0 +1,20 @@
+import {defineField, defineType} from 'sanity'
+import {MdImage} from 'react-icons/md'
+
+export default defineType({
+  title: 'Изображение',
+  name: 'customImage',
+  type: 'image',
+  icon: MdImage,
+  options: {
+    hotspot: true
+  },
+  fields: [
+    defineField({
+      title: 'Описание изображения (alt):',
+      name: 'alt',
+      type: 'string',
+      validation: (Rule) => Rule.error('Описание изображения не может быть пустым!').required()
+    })
+  ]
+})
