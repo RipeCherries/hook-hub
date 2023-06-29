9c36f0a5224e615d4ec7e4f000403f54f5df2269
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-06-29 20:59:03 +0700

    📝 Added a diagram of the article

---
+++ studio/schemas/documents/articles.ts
@@ -0,0 +1,54 @@
+import {defineField, defineType} from 'sanity'
+import {MdLineStyle} from 'react-icons/md'
+import {format} from 'date-fns'
+
+export default defineType({
+  title: 'Статьи',
+  name: 'articles',
+  type: 'document',
+  icon: MdLineStyle,
+  fields: [
+    defineField({
+      title: 'Название статьи:',
+      name: 'title',
+      type: 'string'
+    }),
+    defineField({
+      title: 'Слаг:',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96
+      }
+    }),
+    defineField({
+      title: 'Дата публикации:',
+      name: 'publishedAt',
+      type: 'datetime',
+      options: {
+        dateFormat: 'DD.MM.YYYY'
+      }
+    }),
+    defineField({
+      title: 'Обложка статьи:',
+      name: 'coverImage',
+      type: 'customImage'
+    })
+  ],
+
+  preview: {
+    select: {
+      media: 'coverImage',
+      title: 'title',
+      publishedAt: 'publishedAt'
+    },
+    prepare({media, title, publishedAt}) {
+      return {
+        title,
+        media,
+        subtitle: publishedAt ? format(new Date(publishedAt), 'dd.mm.yyyy') : ''
+      }
+    }
+  }
+})
